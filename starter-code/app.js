462935860ae8a36b1b2447850b6e09c0364f6802
Camille Ory <camilla.ory@gmail.com>
2020-04-02 21:54:55 +0200

    WDPT BER Camille

--- starter-code/app.js
+++ starter-code/app.js
@@ -5,6 +5,8 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
+const strongBeers = punkAPI.getBeers({'abv_gt': 8})
+
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -15,5 +17,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/beers', (req, res) => {
+    punkAPI.getBeers().then(beersFromApi =>{
+        console.log("beeers from api", beersFromApi)
+        res.render("beers",{beers : beersFromApi})
+    })
+});
+
+app.get('/random-beers', (req, res) => {
+    punkAPI.getRandom().then(beersFromApi =>{
+        console.log("Random beeers from api", beersFromApi)
+        res.render("random-beers",{beers : beersFromApi})
+    })
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
